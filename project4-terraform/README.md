4d0f5cfc47b3ab35f3e3c67997de51a680abeaf3
Anil Reddy <anilreddy010910@gmail.com>
2026-04-05 21:44:00 +0530

    Project 4: Terraform AWS Infrastructure

---
+++ project4-terraform/README.md
@@ -0,0 +1,23 @@
+# Project 4 - Terraform AWS Infrastructure
+
+## Tools Used
+- Terraform
+- AWS S3
+- AWS CLI
+
+## What I Built
+Provisioned real AWS cloud infrastructure using
+Terraform (Infrastructure as Code):
+- S3 bucket with versioning enabled
+
+## Terraform Commands Used
+terraform init
+terraform plan
+terraform apply
+terraform destroy
+
+## How to Run
+1. Configure AWS: aws configure
+2. Init: terraform init
+3. Plan: terraform plan
+4. Apply: terraform apply
---
+++ project4-terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "anil-devops-bucket-2026"
+
+  tags = {
+    Name    = "Anil DevOps Bucket"
+    Project = "DevOps Journey"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
